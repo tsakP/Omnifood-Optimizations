07add64962de07c8d16c9def6d873fc797b2aaac
Petros Tsakonas <petros-tsakonas@hotmail.com>
2022-12-13 17:49:49 +0200

    Sticky navbar implemented

--- js/script.js
+++ js/script.js
@@ -36,6 +36,27 @@ allLinks.forEach(function(link) {
   });
 });
 
+//* Sticky Navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(function(entries) {
+  const ent = entries[0];
+  console.log(ent);
+  if(!ent.isIntersecting){
+    document.body.classList.add("sticky");
+  } 
+  if(ent.isIntersecting){
+    document.body.classList.remove("sticky");
+  } 
+}, 
+{
+  //In the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px'
+});
+obs.observe(sectionHeroEl);
+
 ///////////////////////////////////////////////////////////
 //* Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
